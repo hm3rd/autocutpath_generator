5187dbd0b1213751f6d0ee40ba1c10f41b0062bf
hm3rd <hondamikiya@gmail.com>
2026-07-17 11:20:22 +0900

    update
    update README,delete file

--- app_dev/Scripts/activate.bat
+++ app_dev/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\tsuda\AppData\Roaming\inkscape\extensions\test\app_dev
+set VIRTUAL_ENV=C:\Users\tsuda\Downloads\hobby_project\autocutpath_generator\app_dev
 
 if not defined PROMPT set PROMPT=$P$G
 
